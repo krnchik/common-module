aa30ce3e9b5a8d8d785a0c4929ae4f481b4653c1
Matevosyan Karen <mr.mgo@yandex.ru>
2022-10-06 17:21:39 +0300

    Add module core

---
+++ common-core/src/test/java/liga/medical/common/core/CommonApplicationTest.java
@@ -0,0 +1,13 @@
+package liga.medical.common.core;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+public class CommonApplicationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+}
--- common-services/src/main/java/liga/medical/common/service/AnyService.java
+++
@@ -1,7 +0,0 @@
-package liga.medical.common.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class AnyService {
-}
