fa20b5f011551071dcc55045143861043e74de4f
Matevosyan Karen <mr.mgo@yandex.ru>
2022-10-22 17:14:17 +0300

    Add common-core dependency

--- common-core/src/main/java/liga/medical/common/core/CommonApplication.java
+++ common-core/src/main/java/liga/medical/common/core/CommonApplication.java
@@ -2,7 +2,6 @@ package liga.medical.common.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class CommonApplication {
